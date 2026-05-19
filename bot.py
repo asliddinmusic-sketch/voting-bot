37df031eef36fa85134322501285fd3203b0e749
asliddinmusic-sketch <asliddinmusic@gmail.com>
2026-05-19 10:10:15 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -281,12 +281,15 @@ async def cmd_sendpoll(message: Message):
         "✊ Eng munosib nomzodni qo'llab-quvvatlang!"
     )
 
-    markup = InlineKeyboardMarkup(inline_keyboard=[[
+ buttons = []
+for c in CANDIDATES:
+    buttons.append([
         InlineKeyboardButton(
-            text="🗳 Ovoz berish",
-            url="https://t.me/shofirkon_sorovnoma_bot"
+            text=f"{c['name']} | {c['mahalla']}",
+            callback_data=f"select_{c['id']}"
         )
-    ]])
+    ])
+markup = InlineKeyboardMarkup(inline_keyboard=buttons)
 
     await bot.send_photo(
         chat_id=channel,
